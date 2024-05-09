9c1a983878b18aa367aaa1359ec64ac576861353
LaraEvangelista <124074373+LaraEvangelista@users.noreply.github.com>
2024-05-09 16:46:36 +0100

    Update ASB_Assignment_1_Lara_Evangelista.py

--- ASB_Assignment_1_Lara_Evangelista.py
+++ ASB_Assignment_1_Lara_Evangelista.py
@@ -7,8 +7,15 @@ from Bio import Entrez, AlignIO
 
 def query_sequences(database, query):
     
-    """Fetches sequences from a chosen Entrez database, using the given search query, and prints them to the Terminal
-        Takes database and query as input, returns a list of sequences
+    """Fetches sequences from a chosen Entrez database using the given search query.
+
+    Parameters:
+        database (str): The name of the Entrez database to query (e.g., nucleotide, protein, genome, or gene).
+        query (str): The search query used to retrieve sequences from the specified database.
+
+    Returns:
+        str: A list of sequences fetched from the Entrez database in FASTA format.
+
     """
     
     # Set the Entrez email parameter 
@@ -34,8 +41,11 @@ def query_sequences(database, query):
 
 def arguments_parser():
     
-    """Parses the command line arguments, after the user specifies the database and search query
-        Returns the data in a argparse.Namespace object, in this case, argparse.databse, argparse.search_query, and argparse.name_of_file
+    """
+    Parses command line arguments.
+
+    Returns:
+        argparse.Namespace: Parsed command line arguments stored in a Namespace object.
     """
     
     # The support for command-line interfaces is built around an instance of argparse.ArgumentParser. It is a container for argument specifications 
@@ -54,30 +64,86 @@ def arguments_parser():
 
 def align_file(name_of_file):
 
+    """
+    Perform sequence alignment using MAFFT.
+
+    Parameters:
+        name_of_file (str): Name of the input FASTA file containing the sequences to be aligned.
+
+    Returns:
+        str: Name of the output file that contains the aligned sequences.
+    """
+
     base_filename= os.path.splitext(name_of_file)
     output_filename = f"{base_filename[0]}_aligned.fasta"
+
     subprocess.run(f"mafft --auto {name_of_file} > {output_filename}", shell=True, check=True)
+
+    # MAFFT - It's a program for multiple sequence alignment widely used in bioinformatics.
+    # The command used was mafft --auto {name_of_file} > {output_filename}. Meaning of the arguments:
+    # --auto: Specifies the automatic alignment mode, where MAFFT automatically selects the best alignment method based on sequence size.
+    # {name_of_file}: Name of the input FASTA file containing the sequences to be aligned.
+    # >: Redirects the standard output of MAFFT to a file.
+    # {output_filename}: Name of the output file that will contain the aligned sequences.
+
     return output_filename
 
 def Tree_Draw(input_file, tree_file,root):
+
+    """
+    Draws and renders a phylogenetic tree using the RAxML-ng output, a .raxml.bestTree.
+
+    Parameters:
+        input_file (str): Path to the input file containing the tree data.
+        tree_file (str): Name of the output file where the tree visualization will be saved.
+        root (str): Name of the individual to be set as the root of the tree.
+    """
     
     tree = toytree.tree(input_file)
-    #rtre = tree.root(root = root)
-    canvas, axes, mark = tree.draw(width=4000, height=2500)
+    rtree = tree.root(names= root)
+    canvas, axes, mark = rtree.draw(width=4000, height=2500)
     toyplot.html.render(canvas, tree_file)
 
 def Tree_Draw_Bayes(input_file, tree_file,root):
+
+    """
+    Draws and renders a phylogenetic using a mrBayes .con.tre input file.
+
+    Parameters:
+        input_file (str): Path to the input file containing the tree data.
+        tree_file (str): Name of the output file where the tree visualization will be saved.
+        root (str): Name of the individual to be set as the root of the tree.
+    """
     
     tree = toytree.tree(input_file, tree_format=10)
-    #rtre = tree.root(root = root)
-    canvas, axes, mark = tree.draw(width=4000, height=2500)
+    rtree = tree.root(names= root)
+    canvas, axes, mark = rtree.draw(width=4000, height=2500)
     toyplot.html.render(canvas, tree_file)
     
 
 def Model(name_of_aligned_file):
-    #base_filename= os.path.splitext(name_of_aligned_file)
-    #output_filename = f"{base_filename[0]}.fasta"
+
+    """
+    Construct a phylogenetic model using RAxML.
+
+    Parameters:
+        name_of_aligned_file (str): Path to the input file containing the aligned sequences.
+
+    Returns:
+        str: Name of the output file containing the best phylogenetic tree model, ending in .raxml.bestTree.
+    """
+
     process = subprocess.Popen(f"modeltest-ng --force -i {name_of_aligned_file}", shell=True, stdout=subprocess.PIPE)
+
+
+    # ModelTest-NG is a tool used to select the most suitable evolutionary model based on data from a DNA or protein sequence.
+    # Here is the meaning of the arguments:
+
+    # -i {name_of_aligned_file}: This argument indicates the name of the sequence alignment file in FASTA format provided as input to ModelTest-NG. 
+    #  The alignment file contains sequences that have been previously aligned using the MAFFT program.
+
+    # --force: to force the program to overwrite previous files with the same name.
+
     output = process.stdout.readlines()
     for line in output:
         if line.lstrip().startswith(b"> raxml-ng"):
@@ -85,11 +151,26 @@ def Model(name_of_aligned_file):
             model = header[5]
             model_string = model.decode("utf-8")
             break
+    
     subprocess.run(f"raxml-ng --redo --msa {name_of_aligned_file} --model {model_string} --prefix '{name_of_aligned_file}'", shell=True, check=True)
+    
+    # RAxML is a program for maximum likelihood-based phylogenetic inference.
+    # Here is the meaning of the arguments:
+    
+    # --redo: to force the program to overwrite previous files with the same name.
+    
+    # --msa {name_of_aligned_file}: Specifies the file containing the sequence alignment to be used as input for phylogenetic inference.
+    
+    # --model {model_string}: Specifies the evolutionary model to be used for inferring the phylogenetic tree. The model is determined by the 
+    #  previously executed ModelTest-NG program and dynamically obtained by the script.
+    
+    # --prefix '{name_of_aligned_file}': Specifies the prefix for the output files generated by RAxML.
+
 
     return f"{name_of_aligned_file}.raxml.bestTree"
     
 def MrBayes(aligned_file, outgroup):
+
     """
     Converts an aligned FASTA file to Nexus format and appends Mr. Bayes block.
 
@@ -98,8 +179,9 @@ def MrBayes(aligned_file, outgroup):
         outgroup (str): Name of the outgroup for Mr. Bayes.
 
     Returns:
-        str: Path to the output Nexus file.
+        str: Path to the output of the mrBayes command, a .con.tre file.
     """
+
     base_filename = os.path.splitext(aligned_file)[0]
     output_nexus_file = f"{base_filename}.nex"
 
@@ -123,46 +205,46 @@ def MrBayes(aligned_file, outgroup):
             nexus_handle.write(";\n")
             nexus_handle.write("END;\n")
 
+            mrbayes_block = f"""
+                        begin mrbayes; 
+                        set autoclose=yes;
+                        outgroup {outgroup};
+                        mcmcp ngen=150000 printfreq=1000 samplefreq=100 diagnfreq=1000 nchains=4 savebrlens=yes filename={base_filename};
+                        mcmc;
+                        sumt filename={base_filename};
+                        end;
+                        """
+                        
+            nexus_handle.write(mrbayes_block)
+            
             # Append Mr. Bayes block, explained below:
 
-            #begin mrbayes; - This line marks the beginning of the MrBayes block, indicating the start of the MrBayes analysis.
+            # begin mrbayes; - This line marks the beginning of the MrBayes block, indicating the start of the MrBayes analysis.
 
-            #set autoclose=yes; - This line sets the autoclose option to 'yes', which means MrBayes will automatically close 
-            # the analysis once it completes.
+            # set autoclose=yes; - This line sets the autoclose option to 'yes', which means MrBayes will automatically close 
+            #  the analysis once it completes.
 
-            #outgroup {outgroup}; -This line specifies the outgroup for the phylogenetic analysis. The value of {outgroup} is
-            # provided as an argument to the function and represents the name of the outgroup individual.
+            # outgroup {outgroup}; -This line specifies the outgroup for the phylogenetic analysis. The value of {outgroup} is
+            #  provided as an argument to the function and represents the name of the outgroup individual.
 
-            #mcmcp ngen=150000 printfreq=1000 samplefreq=100 diagnfreq=1000 nchains=4 savebrlens=yes filename={base_filename}; -
-            # This line sets various parameters for the Markov chain Monte Carlo (MCMC) process, all explained below:
-            #ngen=150000: Number of generations for the MCMC algorithm.
-            #printfreq=1000: Frequency of printing output to the screen.
-            #samplefreq=100: Frequency of sampling trees.
-            #diagnfreq=1000: Frequency of printing diagnostic information.
-            #nchains=4: Number of MCMC chains to run in parallel.
-            #savebrlens=yes: Indicates whether branch lengths should be saved.
-            #filename={base_filename};: Specifies the base filename for output files generated by MrBayes. 
-            # The value of {base_filename} is derived from the input aligned FASTA file, minus the extension.
+            # mcmcp ngen=150000 printfreq=1000 samplefreq=100 diagnfreq=1000 nchains=4 savebrlens=yes filename={base_filename}; -
+            #  This line sets various parameters for the Markov chain Monte Carlo (MCMC) process, all explained below:
+            # ngen=150000: Number of generations for the MCMC algorithm.
+            # printfreq=1000: Frequency of printing output to the screen.
+            # samplefreq=100: Frequency of sampling trees.
+            # diagnfreq=1000: Frequency of printing diagnostic information.
+            # nchains=4: Number of MCMC chains to run in parallel.
+            # savebrlens=yes: Indicates whether branch lengths should be saved.
+            # filename={base_filename};: Specifies the base filename for output files generated by MrBayes. 
+            #  The value of {base_filename} is derived from the input aligned FASTA file, minus the extension.
 
-            #mcmc; - This line initiates the MCMC process, which is the core algorithm used by MrBayes for phylogenetic inference.
+            # mcmc; - This line initiates the MCMC process, which is the core algorithm used by MrBayes for phylogenetic inference.
 
-            #sumt filename={base_filename}; - This line calculates a consensus tree from the posterior distribution of trees generated 
-            # by the MCMC process. filename={base_filename};: Specifies the filename for the output consensus tree file. The value of 
-            # {base_filename} matches the one used in the mcmcp command.
+            # sumt filename={base_filename}; - This line calculates a consensus tree from the posterior distribution of trees generated 
+            #  by the MCMC process. filename={base_filename};: Specifies the filename for the output consensus tree file. The value of 
+            #  {base_filename} matches the one used in the mcmcp command.
 
-            #end;
-
-            mrbayes_block = f"""
-            begin mrbayes; 
-            set autoclose=yes;
-            outgroup {outgroup};
-            mcmcp ngen=150000 printfreq=1000 samplefreq=100 diagnfreq=1000 nchains=4 savebrlens=yes filename={base_filename};
-            mcmc;
-            sumt filename={base_filename};
-            end;
-            """
             
-            nexus_handle.write(mrbayes_block)
 
     subprocess.run(f"mb {output_nexus_file}", shell=True, check=True)
 
